593d8ede899e986ee5588b83b22040478e5a43bb
Siddhesh-07 <siddheshgk07@gmail.com>
2026-07-05 15:49:47 +0530

    main.tf

--- phase2-state-managment/main.tf
+++ phase2-state-managment/main.tf
@@ -1,12 +1,45 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "phase2" {
-  ami           = "ami-06067086cf86c58e6"
-  instance_type = "t3.micro"
+variable "instance_type" {
+  type    = string
+  default = "t3.micro"
+}
+
+variable "instance_name" {
+  type    = string
+  default = "state-lab-instance"
+}
+
+variable "ami_id" {
+  type    = string
+  default = "ami-06067086cf86c58e6"
+}
+
+resource "aws_instance" "main" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
 
   tags = {
-    Name = "phase2-instance"
+    Name = var.instance_name
   }
+}
+
+output "instance_id" {
+  value = aws_instance.main.id
+}
+
+output "public_ip" {
+  value = aws_instance.main.public_ip
 }
